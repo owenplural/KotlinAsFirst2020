6e55b4c5b52edaa1dced14e8a261b38990055088
Даша <dasha_guseva04@mail.ru>
2021-12-13 00:31:47 +0300

    а

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -223,7 +223,7 @@ fun firstDuplicateIndex(str: String): Int {
     if (list.size < 2) {
         return -1
     } else {
-        for (s in 0..list.size - 1) {
+        for (s in 0..list.size - 2) {
             if (list[s].lowercase() == list[s + 1].lowercase()) {
                 return num
             }
