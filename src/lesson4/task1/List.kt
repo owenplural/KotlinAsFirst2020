14a4a964185d06a1a12e9e22b8f01c63f2cd8068
Даша <dasha_guseva04@mail.ru>
2021-12-13 18:04:50 +0300

    a

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -281,24 +281,24 @@ fun decimalFromString(str: String, base: Int): Int = TODO()
  * 90 = XC, 100 = C, 400 = CD, 500 = D, 900 = CM, 1000 = M.
  * Например: 23 = XXIII, 44 = XLIV, 100 = C
  */
+val romNumb = listOf(
+    1000,
+    900,
+    500,
+    400,
+    100,
+    90,
+    50,
+    40,
+    10,
+    9,
+    5,
+    4,
+    1
+)
+val romAbc = listOf("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
 fun roman(n: Int): String {
     var str = ""
-    val romNumb = listOf(
-        1000,
-        900,
-        500,
-        400,
-        100,
-        90,
-        50,
-        40,
-        10,
-        9,
-        5,
-        4,
-        1
-    )
-    val romAbc = listOf("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
     var n1 = n
     var i = 0
     while (n1 > 0) {
